468a2cbbd7b312b08051f5d8607160d26eec3d85
chaule97 <lebaochau97@gmail.com>
2024-10-10 10:12:48 +0700

    [MIG] mail_thread_create_nolog: Migration to 18.0

--- mail_thread_create_nolog/tests/test_create_nolog.py
+++ mail_thread_create_nolog/tests/test_create_nolog.py
@@ -1,41 +1,50 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.tests.common import SavepointCase
+import json
+
+from odoo.tests.common import tagged
+
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 
 
-class TestMailThreadCreateNoLog(SavepointCase):
+@tagged("post_install", "-at_install")
+class TestMailThreadCreateNoLog(HttpCaseWithUserDemo):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.partner = cls.env["res.partner"].create({"name": "TEST"})
         cls.message_model = cls.env["mail.message"]
 
-    def test_message_fetch_with_record(self):
-        domain = [
-            ("model", "=", self.partner._name),
-            ("res_id", "=", self.partner.id),
-        ]
-        messages = self.message_model.message_fetch(domain)
-        create_msg = messages[-1]
-        # We get a creation message
+    def test_message_fetch(self):
+        session = self.authenticate("demo", "demo")
+
+        response = self.url_open(
+            "/mail/thread/messages",
+            data=json.dumps(
+                {
+                    "params": {
+                        "thread_model": self.partner._name,
+                        "thread_id": self.partner.id,
+                    }
+                }
+            ),
+            headers={
+                "Content-Type": "application/json",
+                "Cookie": f"session_id={session.sid};",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        create_msg = data["result"]["data"]["mail.message"][-1]
+
+        # # We get a creation message
         self.assertEqual(create_msg["model"], self.partner._name)
         self.assertEqual(create_msg["res_id"], self.partner.id)
-        self.assertEqual(create_msg["author_id"][0], self.env.user.partner_id.id)
+        self.assertEqual(create_msg["author"]["id"], self.env.user.partner_id.id)
         self.assertEqual(create_msg["body"], self.partner._creation_message())
         # But it doesn't exist in the DB
         self.assertFalse(self.message_model.browse(create_msg["id"]).exists())
-
-    def test_message_fetch_without_record(self):
-        domain = [
-            ("model", "=", self.partner._name),
-        ]
-        messages = self.message_model.message_fetch(domain)
-        # If we get no message, the test is OK as well
-        if messages:
-            create_msg = messages[-1]
-            # The last message is not the 'creation' one
-            self.assertNotEqual(create_msg["res_id"], self.partner.id)
-            self.assertNotEqual(create_msg["body"], self.partner._creation_message())
-            # And it exists in the DB
-            self.assertTrue(self.message_model.browse(create_msg["id"]).exists())
